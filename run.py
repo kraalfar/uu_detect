822527a0307d5f8ee36e837005a62c9d90e58e46
Farid.Bagirov <farid.bagirov@jetbrains.com>
2022-04-03 23:27:57 +0300

    update run

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
+import sys
+
+sys.path.insert(1, 'utils')
+sys.path.insert(1, 'example')
+sys.path.insert(1, 'example/model')
+
 from example import dataset, nnpu
 from uud_detector import *
 
@@ -11,7 +17,7 @@ def run():
     train_bin = train_holder.pos_neg_split(pos_cls)
     test_bin = test_holder.pos_neg_split(pos_cls)
 
-    for alpha in [0.1, 0.3, 0.5, 0.7, 0.9]:
+    for alpha in [0.05, 0.5, 0.95]:
         train_data, _ = train_bin.get_dataset(alpha, svm_labels=False, size_lab=2500, size_unl=5000)
         test_data, _ = test_bin.get_dataset(alpha, c=0.5, svm_labels=False)
 
@@ -20,7 +26,7 @@ def run():
 
         res = unreliable_data_detector(f, test_data.lab_data(lab=1), test_data.lab_data(lab=0), 0.1)
 
-        print(f"positive proportion={alpha:.2f}. pvalue={res:.4f}", flush=True)
+        print(f"positive proportion={alpha:.2f}. pvalue={res[0]:.4f}", flush=True)
 
 
 if __name__ == '__main__':
